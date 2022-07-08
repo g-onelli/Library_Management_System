008669bee076a9f037b657290683d4c58bdda9b4
gburke2 <aupchvoley20@gmail.com>
2022-07-08 15:51:28 -0400

    main template

--- src/com/main/MenuScreen.java
+++ src/com/main/MenuScreen.java
@@ -1,9 +1,12 @@
 package com.main;
 import java.util.Scanner;
 
+import com.main.db.DB;
 import com.menuCalls.MenuDisplay;
 public class MenuScreen {
 	public static void main(String[] args) {
+		DB db = new DB();
+		db.dbConnect();
 		Scanner loginChoice = new Scanner(System.in);
 		MenuDisplay.mainMenuDisplay();
 		System.out.println("Please choose an option: ");
@@ -12,11 +15,57 @@ public class MenuScreen {
 			MenuDisplay.closingProgram();
 			MenuDisplay.exitMessage();
 		}
-		while(true) {
-			if(loginInput == 1) {
-				//Call switch statement for librarian menu
-			}else {
-				//Call switch statement for patron menu
+		if(loginInput == 1) {
+			//Call switch statement for librarian menu
+			MenuDisplay.libMenuDisplay();
+			int librarianInput = loginChoice.nextInt();
+			while(true){
+				if(librarianInput == 0) {
+					System.out.println("Exiting.. Bye");
+					break;
+				}
+				
+				switch(librarianInput) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				default:
+					break;
+				}
+			}
+		}else {
+			//Call switch statement for patron menu
+			MenuDisplay.patMenuDisplay();
+			int patronInput = loginChoice.nextInt();
+			while(true){
+				if(patronInput == 0) {
+					System.out.println("Exiting.. Bye");
+					break;
+				}
+				
+				switch(patronInput) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				default:
+					break;
+				}
 			}
 		}
 		
